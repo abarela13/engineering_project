296c1203ac27f0bdab84986efa182923c747f55f
Aaron <abarela13@gmail.com>
2021-05-23 18:17:13 -0700

    Best Buy and PG py files completed

---
+++ python/bestbuy.py
@@ -0,0 +1,136 @@
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime as dt
+from fake_useragent import UserAgent
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pg_functions as pg
+import time
+import re
+
+def chrome_driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument("window-size=1600,900")
+    options.add_argument(f'user-agent={UserAgent().chrome}')
+        
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    return webdriver.Chrome(options=options)
+
+def get_soup(url):
+    driver.get(url)
+    time.sleep(2)
+    return BeautifulSoup(driver.page_source, 'html.parser')
+
+def check_products(soup, category, subcategory, drops=[]):
+    sku_list = soup.find('ol', {'class', 'sku-item-list'}).findAll('li', {'class', 'sku-item'})
+
+    for sku in sku_list:
+        title = sku.find('div', {'class', 'sku-title'}).find('a')
+
+        if re.search("combo", title['href']):
+            if sku.find('div', {'class', 'combo-add-to-cart-button'}).find('svg'):
+                link = sku.find('div', {'class', 'sku-title'}).find('a')['href']
+                id = link[-12:]
+                name = title.text.strip()
+                price = sku.find('div', {'class', 'priceView-customer-price'}).find('span').text[1:]
+
+                drops.append([id, name, 2, category, subcategory, price, link, dt.now().strftime('%Y/%m/%d %H:%M:%S'), '', True])
+        else:
+            if sku.find('button', {'class', 'add-to-cart-button'}).find('svg'):
+                id = sku.find('div', {'class', 'sku-title'}).find('a')['href'][-7:]
+                name = title.text.strip()
+                price = sku.find('div', {'class', 'priceView-customer-price'}).find('span').text[1:]
+                link = f'https://api.bestbuy.com/click/-/{id}/pdp'
+                atc = f'https://api.bestbuy.com/click/-/{id}/cart'
+
+                drops.append([id, name, 2, category, subcategory, price, link, dt.now().strftime('%Y/%m/%d %H:%M:%S'), atc, False])
+
+    if soup.find('div', {'class', 'footer-category'}).find_next_sibling('div') and not soup.find('a', {'class', 'sku-list-page-next disabled'}):
+        driver.get(soup.find('a', {'class', 'sku-list-page-next'})['href'])
+        time.sleep(2)
+        return check_products(BeautifulSoup(driver.page_source, 'html.parser'), category, subcategory, drops)
+    else:
+        return drops
+
+def check_product_page(soup, id, category, subcategory, drops=[]):
+    if soup.find('button', {'class', 'btn btn-primary btn-lg btn-block btn-leading-ficon add-to-cart-button'}):
+        name = soup.find('div', {'class', 'sku-title'}).find('h1').text.strip()
+        price = soup.find('div', {'class', 'priceView-customer-price'}).find('span').text[1:]
+        link = f'https://api.bestbuy.com/click/-/{id}/pdp'
+        atc = f'https://api.bestbuy.com/click/-/{id}/cart'
+
+        drops.append([id, name, 2, category, subcategory, price, link, dt.now().strftime('%Y/%m/%d %H:%M:%S'), atc, False])
+    return drops
+
+def check_xbox_consoles(drops):
+    xbox_url = 'https://www.bestbuy.com/site/xbox-series-x-and-s/xbox-series-x-and-s-consoles/pcmcat1586900952752.c?id=pcmcat1586900952752'
+    return check_products(get_soup(xbox_url), 3, 1, drops)
+
+def check_ps5_consoles(drops):
+    ps5_url = 'https://www.bestbuy.com/site/playstation-5/ps5-consoles/pcmcat1587395025973.c?id=pcmcat1587395025973'
+    return check_products(get_soup(ps5_url), 3, 2, drops)
+
+def check_nintendo_switches(drops):
+    nintendo_url = 'https://www.bestbuy.com/site/nintendo-switch/nintendo-switch-consoles/pcmcat1484077694025.c?id=pcmcat1484077694025'
+    return check_products(get_soup(nintendo_url), 3, 3, drops)
+
+def check_rtx_3000_series(drops):
+    rtx_dict = {
+        '3060': 4,
+        '3060 Ti': 5,
+        '3070': 6,
+        '3080': 7,
+        '3090': 8
+        }
+
+    rtx_url = 'https://www.bestbuy.com/site/computer-cards-components/video-graphics-cards/abcat0507002.c?id=abcat0507002&qp=gpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~NVIDIA%20GeForce%20RTX%20{gpu}'
+
+    for gpu in rtx_dict:
+        drops = check_products(get_soup(rtx_url.format(gpu=gpu).replace(' ','%20')), 1, rtx_dict[gpu], drops)
+    return drops
+
+def check_radeon_rx_series(drops):
+    radeon_dict = {
+        '6700 XT': 9,
+        '6800 XT': 10,
+        '6800': 11,
+        '6900 XT': 12
+        }
+
+    radeon_url = 'https://www.bestbuy.com/site/computer-cards-components/video-graphics-cards/abcat0507002.c?id=abcat0507002&qp=gpusv_facet%3DGraphics%20Processing%20Unit%20(GPU)~AMD%20Radeon%20RX%20{gpu}'
+
+    for gpu in radeon_dict:
+        drops = check_products(get_soup(radeon_url.format(gpu=gpu).replace(' ','%20')), 1, radeon_dict[gpu], drops)
+    return drops
+
+def check_ryzen_series(drops=[]):
+    ryzen_dict = {
+        '5600x': ['6438943', 13],
+        '5800X': ['6439000', 14],
+        '5900X': ['6438942', 15],
+        '5950X': ['6438941', 16]
+    }
+
+    product_url = 'https://api.bestbuy.com/click/-/{product}/pdp'
+
+    for gpu in ryzen_dict:
+        drops = check_product_page(get_soup(product_url.format(product=ryzen_dict[gpu][0])), ryzen_dict[gpu][0], 2, ryzen_dict[gpu][1], drops)
+        time.sleep(1)
+    return drops
+
+def check_all_products(drops=[]):
+    drops = check_xbox_consoles(drops)
+    drops = check_ps5_consoles(drops)
+    drops = check_rtx_3000_series(drops)
+    drops = check_radeon_rx_series(drops)
+    drops = check_nintendo_switches(drops)
+    drops = check_ryzen_series(drops)
+    return drops
+
+driver = chrome_driver()
+all_drops = check_all_products()
+pg.process_drops(all_drops)
+driver.close()
---
+++ python/pg_functions.py
@@ -0,0 +1,53 @@
+# Get pandas and postgres to work together
+import datetime
+import pandas.io.sql as pd_sql
+import psycopg2 as pg
+import pandas as pd
+
+# Postgres info to connect
+connection_args = {
+    'host': '192.168.0.242',  # We are connecting to our _local_ version of psql
+    'dbname': 'tracker-network',    # DB that we are connecting to
+    'port': 5432          
+}
+
+connection = pg.connect(**connection_args)
+connection.autocommit = True
+
+# make a cursor
+cursor = connection.cursor()
+
+def product_exists(external_id, vendor_id):
+    query = f"SELECT id FROM products WHERE external_id = '{external_id}'"
+    cursor.execute(query)
+    return cursor.fetchone() is not None
+
+def insert_new_product(external_id, name, vendor_id, category_id, subcategory_id, msrp, product_link, created_at, atc_link, combo):
+    query = f"""
+    INSERT INTO
+        products (external_id, name, vendor_id, category_id, subcategory_id, msrp, product_link, created_at, atc_link, combo)
+    VALUES
+        ('{external_id}', '{name}', {vendor_id}, {category_id}, {subcategory_id}, {msrp}, '{product_link}', '{created_at}', '{atc_link}', {combo})
+    """    
+    cursor.execute(query)
+    
+    return True
+
+def insert_new_drop(external_id, vendor_id, price, created_at):
+    query = f"""
+    INSERT INTO
+        drops (product_id, price, created_at)
+    VALUES
+        ((SELECT id FROM products WHERE external_id = '{external_id}' and vendor_id = {vendor_id}), {price}, '{created_at}')
+    """
+    cursor.execute(query)
+
+    return True
+
+def process_drops(drops):
+    for drop in drops:
+        if not product_exists(drop[0], drop[2]):
+            insert_new_product(drop[0], drop[1], drop[2], drop[3], drop[4], drop[5], drop[6], drop[7], drop[8], drop[9])
+
+        insert_new_drop(drop[0], drop[2], drop[5], drop[7])
+    return True
